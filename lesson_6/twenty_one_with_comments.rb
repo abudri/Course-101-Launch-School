6bebe9a8b62cc210c3fac5c76d97145e735b05af
Abdullah Budri <abdullahbudri@gmail.com>
2018-04-05 12:03:04 -0400

    lesson 6 update

--- lesson_6/twenty_one_with_comments.rb
+++ lesson_6/twenty_one_with_comments.rb
@@ -1,6 +1,23 @@
 require 'pry'
+# 52 card deck comprises of 4 cards each: 2-10, Jack, Queen, King, Ace
+# '2', '3'... '10', 'J', 'Q', 'K', 'A'
+
+=begin
+
+1. Initialize deck
+2. Deal cards to player and dealer
+3. Player turn: hit or stay
+  - repeat until bust or "stay"
+4. If player bust, dealer wins.
+5. Dealer turn: hit or stay
+  - repeat until total >= 17
+6. If dealer bust, player wins.
+7. Compare cards and declare winner.
+
+=end
 
 # ------ CONSTANTS --------
+
 
 SUITS = ['hearts', 'diamonds', 'clubs', 'spades']
 CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
@@ -70,7 +87,7 @@ def display_score(player_cards, dealer_cards)
 end
 
 def determine_winner(player_cards, dealer_cards)
-  return if busted?(player_cards) || busted?(dealer_cards)
+  return if busted?(player_cards) || busted?(dealer_cards) # 'break if' not allowed here, for blocks/loops
   prompt "You win!" if total(player_cards) > total(dealer_cards) 
   prompt "Dealer wins!" if total(dealer_cards) > total(player_cards) 
   prompt "Its a tie!" if total(player_cards) == total(dealer_cards)
@@ -78,15 +95,13 @@ end
 
 # ------- MAIN PROGRAM --------
 
+answer = nil
+answer_play_again = nil
 
-
-loop do # main loop
+loop do 
   deck = initialize_deck
   player_cards = []
   dealer_cards = []
-
-  answer = nil
-  answer_play_again = nil
 
   start_game(deck, player_cards, dealer_cards)
   prompt "#{player_cards} are your two starting cards| Total: #{total(player_cards)}"
@@ -95,7 +110,8 @@ loop do # main loop
   loop do # player turn loop
     puts "hit or stay?"
     answer = gets.chomp
-    break if answer == 'stay' || busted?(player_cards)
+    #binding.pry
+    break if answer == 'stay' || busted?(player_cards)   # the busted? method is not shown
     player_turn(deck, player_cards)
     prompt "#{player_cards} are your cards | Total: #{total(player_cards)}"
     break if busted?(player_cards)
@@ -129,5 +145,28 @@ loop do # main loop
   break if answer_play_again.start_with?('n')
 
 end
+# ... continue on to Dealer turn
 
 prompt "Thanks for playing Twenty-One!"
+
+
+# player_score = 0
+# dealer_score = 0
+
+
+
+
+#loop do # main loop
+#  deck = initialize_deck
+# end
+
+# DEALER TURN LOOP
+#  BREAK if busted?(player_cards) OR if initial dealer total score >= 17
+# stay --> leave loop
+# ELSE dealer_turn(deck, dealer) for another card
+# if total for dealer >=17, leave loop
+# else dealer turn
+
+
+
+
